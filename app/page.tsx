5792ac15ee1700b97f96a70331ecef84559df522
holabayor <liasu.olabayo@gmail.com>
2023-06-29 16:03:17 +0100

    headless ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Hero } from "@/components"
+import { CustomFilter, Hero, SearchBar } from "@/components"
 
 
 export default function Home() {
@@ -6,6 +6,24 @@ export default function Home() {
     <main
       className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl">
+            Car Catalogue
+          </h1>
+          <p>
+            Explore the cars you might like
+          </p>
+
+          <div className="home__filters">
+            <SearchBar />
+            <div className="home__filter-container">
+              <CustomFilter title="fuel" />
+              <CustomFilter title="year" />
+              S</div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
