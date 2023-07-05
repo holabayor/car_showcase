984cac6d74274b38108ef186b133f7c9f9119a49
holabayor <liasu.olabayo@gmail.com>
2023-07-05 22:38:24 +0100

    car card

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -26,20 +26,19 @@ const CarCard = ({ car }: CarCardProps) => {
                     {make} {model}
                 </h2>
             </div>
+
             <p className="flex mt-6 text-[32px] font-extrabold">
-                <span className="self-start text-[14px] font-semibold">
-                    $
-                </span>
+                <span className="self-start text-[14px] font-semibold">$</span>
                 {carRent}
-                <span className="self-end text-[14px] font-semibold">
-                    /day
-                </span>
+                <span className="self-end text-[14px] font-semibold">/day</span>
             </p>
+
             <div className="relative w-full h-40 my-3 object-contain">
                 <Image src="/hero.png" alt="car" fill priority className="object-contain" />
             </div>
-            <div className="relative w-full h-40 mt-2">
-                <div className="flex group-hover:invisible w-full justify-between text-gray">
+
+            <div className="relative flex w-full h-40 mt-2">
+                <div className="flex group-hover:invisible w-full justify-between text-grey">
                     <div
                         className="flex flex-col justify-center items-center gap-2">
                         <Image src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
@@ -50,16 +49,12 @@ const CarCard = ({ car }: CarCardProps) => {
                     <div
                         className="flex flex-col justify-center items-center gap-2">
                         <Image src="/tire.svg" width={20} height={20} alt="tire" />
-                        <p className="text-[14px]">
-                            {drive.toUpperCase()}
-                        </p>
+                        <p className="text-[14px]">{drive.toUpperCase()}</p>
                     </div>
                     <div
                         className="flex flex-col justify-center items-center gap-2">
                         <Image src="/gas.svg" width={20} height={20} alt="gas" />
-                        <p className="text-[14px]">
-                            {city_mpg} MPG
-                        </p>
+                        <p className="text-[14px]">{city_mpg} MPG</p>
                     </div>
                 </div>
 
