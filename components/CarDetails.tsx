91a45c0abaddfa38f151eb941f735c5b4b58d43f
holabayor <liasu.olabayo@gmail.com>
2023-07-06 21:18:05 +0100

    car images

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -41,7 +41,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                 leaveTo="opacity-0 scale-95"
                             >
                                 <Dialog.Panel
-                                    className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadows-xsl transition-all flex flex-col gap-5">
+                                    className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                                     <button
                                         type="button"
                                         className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
@@ -67,6 +67,15 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                         <h2 className="font-semibold text-xl capitalize">
                                             {car.make} {car.model}
                                         </h2>
+                                        <div className="mt-3 flex flex-wrap gap-4">
+                                            {Object.entries(car).map(([key, value]) => (
+                                                <div className="flex justify-between gap-5 w-full text-right"
+                                                    key={key}>
+                                                    <h4 className="text-grey capitalize">{key.split("_").join(" ")}</h4>
+                                                    <p className="text-black-100 font-semibold">{value}</p>
+                                                </div>
+                                            ))}
+                                        </div>
                                     </div>
                                 </Dialog.Panel>
                             </Transition.Child>
