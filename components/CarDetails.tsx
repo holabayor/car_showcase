8fa44112b85631429b97b568a4efb50721d6eb9a
holabayor <liasu.olabayo@gmail.com>
2023-07-14 23:51:03 +0100

    cdn fix

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { Dialog, Transition } from "@headlessui/react";
 
 import { CarProps } from "@/types";
+import { generateCarImageUrl } from "@/utils";
 
 interface CarDetailsProps {
     isOpen: boolean;
@@ -50,15 +51,15 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                                     </button>
                                     <div className="flex-1 flex flex-col gap-3">
                                         <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg">
-                                            <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                            <Image src={generateCarImageUrl(car)} alt="car" fill priority className="object-contain" />
                                         </div>
                                         <div className="flex gap-3">
                                             <div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                                <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                                <Image src={generateCarImageUrl(car, "29")} alt="car" fill priority className="object-contain" />
                                             </div><div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                                <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                                <Image src={generateCarImageUrl(car, "33")} alt="car" fill priority className="object-contain" />
                                             </div><div className="flex-1 relative w-full h-24 bg-primary-blue-100 rounded-lg">
-                                                <Image src="/hero.png" alt="car" fill priority className="object-contain" />
+                                                <Image src={generateCarImageUrl(car, "13")} alt="car" fill priority className="object-contain" />
                                             </div>
                                         </div>
                                     </div>
