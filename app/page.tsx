68d4c12d7565d1418ec5f7e952922d4b3d791481
holabayor <liasu.olabayo@gmail.com>
2023-07-14 23:42:40 +0100

    server side rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,52 +1,17 @@
-"use client";
-
 import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components"
 import { fetchCars } from "@/utils"
 import { fuels, yearsOfProduction } from "@/constants";
-import { useEffect, useState } from "react";
-import Image from "next/image";
+import { HomeProps } from "@/types";
 
 
-export default async function Home() {
-  // default states
-  const [allCars, setAllCars] = useState([]);
-  const [loading, setLoading] = useState(false)
-
-  // search states
-  const [manufacturer, setManufacturer] = useState("");
-  const [model, setModel] = useState("");
-
-  // filter states
-  const [fuel, setFuel] = useState("");
-  const [year, setYear] = useState(2022);
-
-  // pagination states
-  const [limit, setLimit] = useState(10);
-
-  const getCars = async () => {
-    setLoading(true);
-    try {
-      const result = await fetchCars({
-        manufacturer: manufacturer || '',
-        year: year || 2022,
-        fuel: fuel || '',
-        limit: limit || 10,
-        model: model || '',
-      });
-
-      setAllCars(result);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-
-  useEffect(() => {
-
-  }, [fuel, year, limit, manufacturer, model])
-
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -65,14 +30,14 @@ export default async function Home() {
         </div>
 
         <div className="home__filters">
-          <SearchBar setManfacturer={setManufacturer} setModel={setModel} />
+          <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} setFilter={setFuel} />
-            <CustomFilter title="year" options={yearsOfProduction} setFilter={setYear} />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
-        {allCars.length > 0 ?
+        {!isDataEmpty ?
           (
             <section>
               <div className="home__cars-wrapper">
@@ -80,21 +45,10 @@ export default async function Home() {
                   <CarCard car={car} />
                 ))}
               </div>
-              {loading && (
-                <div className="mt-15 w-full flex-center">
-                  <Image
-                    src="loader.svg"
-                    width={50}
-                    height={50}
-                    className="object-contain"
-                    alt="loader"
-                  />
-                </div>
-              )}
+
               <ShowMore
-                pageNumber={limit / 10}
-                isNext={limit > allCars.length}
-                setLimit={setLimit}
+                pageNumber={(searchParams.limit || 10) / 10}
+                isNext={(searchParams.limit || 10) > allCars.length}
               />
             </section>
           ) : (
--- utils/index.ts
+++ utils/index.ts
@@ -15,7 +15,6 @@ export async function fetchCars(filters: FilterProps) {
   try {
     const response = await fetch(url, options);
     const result = await response.json();
-    console.log(result);
     return result;
   } catch (error) {
     console.error(error);
